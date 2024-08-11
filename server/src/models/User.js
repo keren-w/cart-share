173fcd7c7f0d2f02963469560d340062f641352f
Keren Wigelman <keren.w@gmail.com>
2024-08-11 21:25:02 +0300

    user schema fix

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     '_id': { type: mongoose.Schema.Types.ObjectId},
-    name: { type: String, required: true, unique: true },
+    name: { 
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true }
+     },
     activeCarts: [{
         '_id': { type: mongoose.Schema.Types.ObjectId, ref: 'Cart' },
         isOwner: { type: Boolean, default: false }
