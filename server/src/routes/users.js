3feb0674a8111ad3805d6e2a0541563da0741b07
Keren Wigelman <keren.w@gmail.com>
2024-08-12 22:00:35 +0300

    controller with 2 l's

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const login = require('../controllers/users.controlller');
+const login = require('../controllers/users.controller');
 const User = require('../models/User');
 
 router.get('/', (req, res) => {
